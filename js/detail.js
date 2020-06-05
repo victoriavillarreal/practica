0ea0e1ad15270b8954ab5c12b94ac05a71696173
catalinaarias01 <carias@udesa.edu.ar>
2020-06-05 18:32:09 -0300

    Detail

--- js/detail.js
+++ js/detail.js
@@ -3,244 +3,6 @@ window.onload = function(){
 var queryString = location.search;
 var queryStringObj = new URLSearchParams(queryString);
 
-function traemeCancion(detailCancionId){
-
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/" + detailCancionId)
-
-   .then (function (respuesta) {
-       return respuesta.json();
-   })
-   .then (function (informacionDetailCancion) {
-       console.log(informacionDetailCancion);
-
-       var contenedorTracks = document.querySelector(".containertracks");
-       
-       var contenidoTracks = "";
-
-       for (let i = 0; i < informacionDetailCancion.length ; i++) {
-           var element = informacionDetailCancion[i];
-           
-
-           contenidoTracks += "<div class='body bodytrack'>";
-           contenidoTracks += "<div class='lasfotos'>";
-           contenidoTracks += "<div class='cancion'>";
-           contenidoTracks += "<h2>" + element.contributors.title + "</h2>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='fotos'>";
-           contenidoTracks += "<div class='dato'>";
-           contenidoTracks += "<img src='" + element.contributors.picture_big + "' alt='' class='secciones margin ftrack' >";
-           contenidoTracks += "<p class='track'> Portada </p>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='dato'>";
-           contenidoTracks += "<img src='" + element.album.picture_big + "' alt='' class='secciones margin ftrack' >";
-           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='losdetalles'>";
-           contenidoTracks += "<div class='fecha'>";
-           contenidoTracks += "<p> Release date:" + element.release_date +  "</p>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
-           contenidoTracks += "<a href='playlist.html'><p>Add to playlist</p></a> ";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-       }
-
-       contenedorTracks.innerHTML = contenidoTracks;
-
-   })
-}function traemeCancion(detailCancionId){
-
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/" + detailCancionId)
-
-   .then (function (respuesta) {
-       return respuesta.json();
-   })
-   .then (function (informacionDetailCancion) {
-       console.log(informacionDetailCancion);
-
-       var contenedorTracks = document.querySelector(".containertracks");
-       
-       var contenidoTracks = "";
-
-       for (let i = 0; i < informacionDetailCancion.length ; i++) {
-           var element = informacionDetailCancion[i];
-           
-
-           contenidoTracks += "<div class='body bodytrack'>";
-           contenidoTracks += "<div class='lasfotos'>";
-           contenidoTracks += "<div class='cancion'>";
-           contenidoTracks += "<h2>" + element.contributors.title + "</h2>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='fotos'>";
-           contenidoTracks += "<div class='dato'>";
-           contenidoTracks += "<img src='" + element.contributors.picture_big + "' alt='' class='secciones margin ftrack' >";
-           contenidoTracks += "<p class='track'> Portada </p>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='dato'>";
-           contenidoTracks += "<img src='" + element.album.picture_big + "' alt='' class='secciones margin ftrack' >";
-           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='losdetalles'>";
-           contenidoTracks += "<div class='fecha'>";
-           contenidoTracks += "<p> Release date:" + element.release_date +  "</p>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
-           contenidoTracks += "<a href='playlist.html'><p>Add to playlist</p></a> ";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-       }
-
-       contenedorTracks.innerHTML = contenidoTracks;
-
-   })
-}function traemeCancion(detailCancionId){
-
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/" + detailCancionId)
-
-   .then (function (respuesta) {
-       return respuesta.json();
-   })
-   .then (function (informacionDetailCancion) {
-       console.log(informacionDetailCancion);
-
-       var contenedorTracks = document.querySelector(".containertracks");
-       
-       var contenidoTracks = "";
-
-       for (let i = 0; i < informacionDetailCancion.length ; i++) {
-           var element = informacionDetailCancion[i];
-           
-
-           contenidoTracks += "<div class='body bodytrack'>";
-           contenidoTracks += "<div class='lasfotos'>";
-           contenidoTracks += "<div class='cancion'>";
-           contenidoTracks += "<h2>" + element.contributors.title + "</h2>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='fotos'>";
-           contenidoTracks += "<div class='dato'>";
-           contenidoTracks += "<img src='" + element.contributors.picture_big + "' alt='' class='secciones margin ftrack' >";
-           contenidoTracks += "<p class='track'> Portada </p>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='dato'>";
-           contenidoTracks += "<img src='" + element.album.picture_big + "' alt='' class='secciones margin ftrack' >";
-           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='losdetalles'>";
-           contenidoTracks += "<div class='fecha'>";
-           contenidoTracks += "<p> Release date:" + element.release_date +  "</p>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
-           contenidoTracks += "<a href='playlist.html'><p>Add to playlist</p></a> ";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-       }
-
-       contenedorTracks.innerHTML = contenidoTracks;
-
-   })
-}function traemeCancion(detailCancionId){
-
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/" + detailCancionId)
-
-   .then (function (respuesta) {
-       return respuesta.json();
-   })
-   .then (function (informacionDetailCancion) {
-       console.log(informacionDetailCancion);
-
-       var contenedorTracks = document.querySelector(".containertracks");
-       
-       var contenidoTracks = "";
-
-       for (let i = 0; i < informacionDetailCancion.length ; i++) {
-           var element = informacionDetailCancion[i];
-           
-
-           contenidoTracks += "<div class='body bodytrack'>";
-           contenidoTracks += "<div class='lasfotos'>";
-           contenidoTracks += "<div class='cancion'>";
-           contenidoTracks += "<h2>" + element.contributors.title + "</h2>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='fotos'>";
-           contenidoTracks += "<div class='dato'>";
-           contenidoTracks += "<img src='" + element.contributors.picture_big + "' alt='' class='secciones margin ftrack' >";
-           contenidoTracks += "<p class='track'> Portada </p>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='dato'>";
-           contenidoTracks += "<img src='" + element.album.picture_big + "' alt='' class='secciones margin ftrack' >";
-           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='losdetalles'>";
-           contenidoTracks += "<div class='fecha'>";
-           contenidoTracks += "<p> Release date:" + element.release_date +  "</p>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
-           contenidoTracks += "<a href='playlist.html'><p>Add to playlist</p></a> ";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-       }
-
-       contenedorTracks.innerHTML = contenidoTracks;
-
-   })
-}
-function traemeCancion(detailCancionId){
-
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/" + detailCancionId)
-
-   .then (function (respuesta) {
-       return respuesta.json();
-   })
-   .then (function (informacionDetailCancion) {
-       console.log(informacionDetailCancion);
-
-       var contenedorTracks = document.querySelector(".containertracks");
-       
-       var contenidoTracks = "";
-
-       for (let i = 0; i < informacionDetailCancion.length ; i++) {
-           var element = informacionDetailCancion[i];
-           
-
-           contenidoTracks += "<div class='body bodytrack'>";
-           contenidoTracks += "<div class='lasfotos'>";
-           contenidoTracks += "<div class='cancion'>";
-           contenidoTracks += "<h2>" + element.contributors.title + "</h2>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='fotos'>";
-           contenidoTracks += "<div class='dato'>";
-           contenidoTracks += "<img src='" + element.contributors.picture_big + "' alt='' class='secciones margin ftrack' >";
-           contenidoTracks += "<p class='track'> Portada </p>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='dato'>";
-           contenidoTracks += "<img src='" + element.album.picture_big + "' alt='' class='secciones margin ftrack' >";
-           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<div class='losdetalles'>";
-           contenidoTracks += "<div class='fecha'>";
-           contenidoTracks += "<p> Release date:" + element.release_date +  "</p>";
-           contenidoTracks += "</div>";
-           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
-           contenidoTracks += "<a href='playlist.html'><p>Add to playlist</p></a> ";
-           contenidoTracks += "</div>";
-           contenidoTracks += "</div>";
-       }
-
-       contenedorTracks.innerHTML = contenidoTracks;
-
-   })
-}
-
 
 if(queryStringObj.has("cancionId")){
     var detailCancionId = queryStringObj.get("cancionId");
@@ -261,6 +23,55 @@ else{
 
 console.log(detailCancionId);
 
+
+function traemeCancion(detailCancionId){
+
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/" + detailCancionId)
+
+   .then (function (respuesta) {
+       return respuesta.json();
+   })
+   .then (function (informacionDetailCancion) {
+       console.log(informacionDetailCancion);
+
+       var contenedorTracks = document.querySelector(".containertracks");
+       
+       var contenidoTracks = "";
+
+       for (let i = 0; i < informacionDetailCancion.length ; i++) {
+           var element = informacionDetailCancion[i];
+           
+
+           contenidoTracks += "<div class='body bodytrack'>";
+           contenidoTracks += "<div class='lasfotos'>";
+           contenidoTracks += "<div class='cancion'>";
+           contenidoTracks += "<h2>" + element.contributors.title + "</h2>";
+           contenidoTracks += "</div>";
+           contenidoTracks += "<div class='fotos'>";
+           contenidoTracks += "<div class='dato'>";
+           contenidoTracks += "<img src='" + element.contributors.picture_big + "' alt='' class='secciones margin ftrack' >";
+           contenidoTracks += "<p class='track'> Portada </p>";
+           contenidoTracks += "</div>";
+           contenidoTracks += "<div class='dato'>";
+           contenidoTracks += "<img src='" + element.album.picture_big + "' alt='' class='secciones margin ftrack' >";
+           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
+           contenidoTracks += "</div>";
+           contenidoTracks += "</div>";
+           contenidoTracks += "</div>";
+           contenidoTracks += "<div class='losdetalles'>";
+           contenidoTracks += "<div class='fecha'>";
+           contenidoTracks += "<p> Release date:" + element.release_date +  "</p>";
+           contenidoTracks += "</div>";
+           contenidoTracks += "<a href='detail.html?albumId=" + albumId +"'> Album:" + element.album.title + "</a>";
+           contenidoTracks += "<a href='playlist.html'><p>Add to playlist</p></a> ";
+           contenidoTracks += "</div>";
+           contenidoTracks += "</div>";
+       }
+
+       contenedorTracks.innerHTML = contenidoTracks;
+
+   })
+}
 
 
 function traemeAlbum(detailAlbumId){
